0e184d625b861e21da1b344014c4cd81df08ac6b
Haim365 <hguel@grupo365.mx>
2025-03-30 00:02:55 -0600

    Add card list to implement isolated container

---
+++ src/app/card-list/card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
+@Component({
+    selector:  'card-component',
+    templateUrl: "./card-list.component.html",
+    imports: [CommonModule],
+    standalone: true,
+})
+
+export class CardListComponent{
+
+}
